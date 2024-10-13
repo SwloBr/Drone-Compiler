a8831d884398736425e35c80b660555f67835a10
SwloBr <ppaulo2003.ppsr@gmail.com>
2024-10-13 09:21:33 -0300

    :recycle: refactor: path to absolute for easy test

--- src/com/swlo/Main.java
+++ src/com/swlo/Main.java
@@ -3,12 +3,13 @@ package com.swlo;
 import com.swlo.lex.Analyser;
 import com.swlo.token.Token;
 
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         // Exemplo de uso do Analyser com arquivo de entrada
-        String filePath = "/home/swlo/src/Compiladores/Test.drone";
+        String filePath = Paths.get("Test.drone").toAbsolutePath().toString();
         Analyser lexer = new Analyser(filePath);
         List<Token> tokens = lexer.tokenize();
         tokens.forEach(token -> System.out.println(token.toString()));
